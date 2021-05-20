c5da42c14721d6fed55c2a69bfb618246990d0ca
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-05-21 00:56:22 +0100

    css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import '../style/Header.css'
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+
 const Header = () => {
     return (
-        <nav className="header">
+        <nav className="header" >
             {/* Logo on the left */}
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG25.png" alt='AmazonLogo' />
+            <Link to="/">
+                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG25.png" alt='AmazonLogo' />
+            </Link>
 
             {/* Search Box */}
+            <div className="header__search">
+                <input type="search" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
 
             {/* 3 Links */}
 
