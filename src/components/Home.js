1eb9be5e09f558cf0fe936a0f5d8ca48a1c6c262
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-05-21 22:46:42 +0100

    update products

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../style/Home.css'
+import Product from './Product'
 
 const Home = () => {
     return (
@@ -7,7 +8,20 @@ const Home = () => {
             <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/launch/gateway/TheUndergroundRailroad/UGRR_S1_GWBleedingHero_POST_XSite_3000x1200_PV_en-GB._CB669781769_.jpg"
                 alt="amazonImage" />
 
+
+            <Product
+                id={1}
+                title='FireStick'
+                price={21.99}
+                image='https://images-eu.ssl-images-amazon.com/images/I/51Da2Z%2BFTFL._AC_UL480_SR480,480_.jpg'
+                rating={5}
+            />
+
+
         </div>
+
+
+
     )
 }
 
