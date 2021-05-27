93ee09a5ea1f9865f39a1b1df770928a518a6881
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-05-27 23:13:52 +0100

    updating

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,20 @@
-import React from 'react'
 import '../style/Login.css'
+import React, { useState } from 'react';
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./Firebase";
 
 const Login = () => {
+    const history = useHistory();
+
+
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className='login'>
+            <Link to='/'>
+                <img
+                    className="login__logo"
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
+                />
+            </Link>
         </div>
     )
 }
