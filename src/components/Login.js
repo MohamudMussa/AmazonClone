adaf4d56c47456931ef5f1e363f0e2da84ab4168
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-05-27 23:20:43 +0100

    styled the amazon sign in page

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,16 @@ import { auth } from "./Firebase";
 
 const Login = () => {
     const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
+    const register = () => {
+
+    }
+
+    const signIn = () => {
+
+    }
 
     return (
         <div className='login'>
@@ -15,7 +24,36 @@ const Login = () => {
                     src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
                 />
             </Link>
+            <div className='login__container'>
+                <h1>Sign-in</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)} />
+
+                    <h5>Password</h5>
+                    <input type='password'
+                        value={password}
+                        onChange={e => setPassword(e.target.value)} />
+
+                    <button type='submit'
+                        onClick={signIn}
+                        className='login__signInButton'>Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+                <p>By Mohamud Mussa 🌹</p>
+
+                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
+            </div>
         </div>
+
     )
 }
 
